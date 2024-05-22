83373a43f58cea24dd0b45d13426613d8f0a9523
ioana-manghiuc <ioana-manghiuc@users.noreply.github.com>
2024-05-22 15:11:05 +0300

    Created DALHelper & DAL classes for accessing stored procedures.

---
+++ convenience_store_/convenience_store_/Models/DataAccessLayer/DALHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+using convenience_store_.Models;
+
+namespace convenience_store_.Models.DataAccessLayer
+{
+    public static class DALHelper
+    {
+        private static readonly string connectionString = ConfigurationManager.ConnectionStrings["myConStr"].ConnectionString;
+
+        public static SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(connectionString);
+            }
+        }
+    }
+}
--- convenience_store_/convenience_store_/Models/EntityLayer/Product.cs
+++ convenience_store_/convenience_store_/Models/EntityLayer/Product.cs
@@ -7,6 +7,16 @@ using convenience_store_.Models.EntityLayer;
 
 namespace convenience_store_.Models
 {
+    public enum ECategory
+    {
+        Food,
+        Beverage,
+        Household,
+        PersonalCare,
+        Other,
+        None
+    }
+
     public class Product: BasePropertyChanged
     {
         // ID, Name, Barcode, ExpirationDate, Category, ManufacturerID, IsActive
@@ -62,6 +72,23 @@ namespace convenience_store_.Models
             set
             {
                 category = value;
+                NotifyPropertyChanged("Category");
+            }
+        }
+
+        public ECategory CategoryEnum
+        {
+            get
+            {
+                if (Enum.TryParse(category, out ECategory parsedCategory))
+                {
+                    return parsedCategory;
+                }
+                return ECategory.None;
+            }
+            set
+            {
+                category = value.ToString();
                 NotifyPropertyChanged("Category");
             }
         }
